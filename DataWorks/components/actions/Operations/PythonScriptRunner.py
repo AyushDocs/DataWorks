7ef8d8dea3484bf1a9797bd9d0376858509059b0
AyushDocs <ayushalokdubey@gmail.com>
2025-02-01 19:28:06 +0530

    fixed error due to wrong script variable name

--- DataWorks/components/actions/Operations/PythonScriptRunner.py
+++ DataWorks/components/actions/Operations/PythonScriptRunner.py
@@ -8,16 +8,16 @@ class PythonScriptRunner:
     python_script_url: str
 
     def execute(self):
-        if not self.script_url.endswith(".py"):
+        if not self.python_script_url.endswith(".py"):
             logging.error("Invalid script URL: Not a Python file")
             return
-        logging.info(f"Running script {self.script_url}")
+        logging.info(f"Running script {self.python_script_url}")
         logging.warning(
-            f"Running script {self.script_url} which may potentially harm the system"
+            f"Running script {self.python_script_url} which may potentially harm the system"
         )
         subprocess.run(
-            f"uv run {self.script_url} 24f2004275@ds.study.iitm.ac.in",
+            f"uv run {self.python_script_url} 24f2004275@ds.study.iitm.ac.in",
             shell=True,
             check=True,
         )
-        logging.info(f"Ran script {self.script_url}")
+        logging.info(f"Ran script {self.python_script_url}")
