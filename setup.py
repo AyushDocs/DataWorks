e328e16caf0a9777cd6436bee50ad143eddeb9a2
AyushDocs <ayushalokdubey@gmail.com>
2025-02-01 18:19:10 +0530

    setup file added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'DataWorks',
+    version= '0.0.0',
+    author= 'Ayush',
+    author_email= 'ayushalokdubey@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
